2ff600c728324e38ee1ffcfebcdd4b2f5f4c3a4e
Wilke Bouwer <wilke@wilkebouwer.nl>
2022-05-18 10:32:02 +0200

    Add letters beginning

--- main.js
+++ main.js
@@ -13,8 +13,33 @@ function createBlock() {
     return gameBlock;
 }
 
+function getNewRandomValueFromArray(currentArray, randomArray) {
+	let cardValue = randomArray[Math.floor(Math.random() * randomArray.length)];
+
+	if (currentArray.indexOf(cardValue) != -1) {
+		return getNewRandomValueFromArray(currentArray, randomArray);
+	}
+	return cardValue;
+}
+
+const alphabet = ['a', 'b', 'c', 'd', 'e',
+	          'f', 'g', 'h', 'i', 'j',
+                  'k', 'l', 'm', 'n', 'o',
+                  'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y',
+                  'z'];
+
+let letterArray = [];
+
+for (let index = 0; index < Math.pow(6, 2) / 2; index++) {
+	let cardValue = getNewRandomValueFromArray(letterArray, alphabet);
+    	letterArray.push(cardValue);
+}
+
 for (let index = 0; index < Math.pow(6, 2); index++) {
     let block = createBlock();
+
+    block.innerText = cardValue;
 
     if (Math.random() < 0.15) {
         block.classList.add("selected");
