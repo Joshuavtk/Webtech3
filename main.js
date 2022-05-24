8dd898dcaedc6d2bded0a324a9a37daea9fa9e74
Joshua <joshuavtk@hotmail.com>
2022-05-24 21:22:37 +0200

    Add animations and playable game

--- main.js
+++ main.js
@@ -5,50 +5,66 @@ const gameOptions = {
 
 const gameContainer = document.querySelector(".game");
 
-function createBlock(value) {
+function createBlock(value, index) {
     let gameBlock = document.createElement("div");
     gameBlock.classList.add("game_block");
+    gameBlock.setAttribute("data-id", index)
 
-    let gameBlockText = document.createElement("div");
-    gameBlockText.innerText = value;
-    gameBlockText.classList.add("block_inner");
+    let gameBlockInner = document.createElement("div");
+    gameBlockInner.classList.add("block_inner");
 
-    gameBlock.appendChild(gameBlockText);
+    gameBlock.appendChild(gameBlockInner);
+
+    let blockValue = document.createElement("div");
+    blockValue.innerText = value;
+    blockValue.classList.add("value");
+
+    let blockPlaceholder = document.createElement("div");
+    blockPlaceholder.innerText = "*";
+    blockPlaceholder.classList.add("placeholder");
+
+    gameBlockInner.appendChild(blockValue);
+    gameBlockInner.appendChild(blockPlaceholder);
+
 
     return gameBlock;
 }
 
 function getNewRandomValueFromArray(currentArray, randomArray) {
-	let cardValue = randomArray[Math.floor(Math.random() * randomArray.length)];
+    let cardValue = randomArray[Math.floor(Math.random() * randomArray.length)];
 
-	if (currentArray.indexOf(cardValue) != -1) {
-		return getNewRandomValueFromArray(currentArray, randomArray);
-	}
-	return cardValue;
+    // console.log(currentArray);
+    for (const val of currentArray) {
+        // console.log(val[0] == cardValue);
+        if (val[0] == cardValue) {
+            return getNewRandomValueFromArray(currentArray, randomArray);
+        }
+    }
+    return cardValue;
 }
 
 const alphabet = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
 let letterArray = [];
 
 for (let index = 0; index < Math.pow(6, 2) / 2; index++) {
-	let cardValue = getNewRandomValueFromArray(letterArray, alphabet);
-    	letterArray.push(cardValue);
+    let cardValue = getNewRandomValueFromArray(letterArray, alphabet);
+    letterArray.push([cardValue, index]);
 }
 
 letterArray.push(...letterArray)
 
 
 letterArray = letterArray
-  .map(value => ({ value, sort: Math.random() }))
-  .sort((a, b) => a.sort - b.sort)
-  .map(({ value }) => value)
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
 
 for (let index = 0; index < Math.pow(6, 2); index++) {
-    let block = createBlock(letterArray[index]);
+    let block = createBlock(letterArray[index][0], letterArray[index][1]);
 
     gameContainer.append(block);
 }
@@ -58,15 +74,37 @@ let currentlySelectedBlock = null;
 
 document.querySelector('.game').addEventListener("click", el => {
     let selectedBlock = el.target
-    if (selectedBlock.classList.contains("game")) return
+
+    if (!(selectedBlock.classList.contains("game_block")
+        || selectedBlock.classList.contains("block_inner")
+        || selectedBlock.classList.contains("placeholder"))) {
+        return
+    }
+
     if (selectedBlock.classList.contains("block_inner")) {
         selectedBlock = selectedBlock.parentElement
     }
+    if (selectedBlock.classList.contains("placeholder")) {
+        selectedBlock = selectedBlock.parentElement.parentElement
+    }
+    if (selectedBlock.classList.contains("selected") || selectedBlock.classList.contains("correct")) return
+
+    selectedBlock.classList.add('selected')
 
     if (currentlySelectedBlock) {
-        if (currentlySelectedBlock.innerText == selectedBlock.innerText) {
+        if (currentlySelectedBlock == selectedBlock.dataset.id) {
+            console.log(el.target);
             console.log("match");
+            document.querySelectorAll(`div[data-id="${currentlySelectedBlock}"]`)
+                .forEach(el => el.classList.add('correct'))
+        } else {
+            console.log('geen match');
         }
+        setTimeout(() => {
+            document.querySelectorAll(".selected").forEach(el => el.classList.remove('selected'))
+        }, 1000)
+        currentlySelectedBlock = null
+    } else {
+        currentlySelectedBlock = selectedBlock.dataset.id
     }
-    console.log(selectedBlock);
 })
